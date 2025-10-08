224d926afd7f5d26b4bc1dc890df5368f260c476
irenejimmy <irenejimmy2028@cs.sjcetpalai.ac.in>
2025-10-08 10:30:40 +0530

    Add files via upload

---
+++ Basics16.java
@@ -0,0 +1,35 @@
+package irene;
+import javax.swing.*;
+import java.awt.*;
+public class Basics16 {
+
+	public static void main(String[] args) {
+		JFrame mainFrame=new JFrame();
+		mainFrame.setSize(300,150);
+		mainFrame.setTitle("The first GUI");
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(3,2,10,10));
+		JLabel userLabel = new JLabel("User Name");
+		JTextField userField = new JTextField();
+		JLabel pwdLabel = new JLabel("Password");
+		JPasswordField pwdField = new JPasswordField();
+		JButton loginbtn = new JButton("Login");
+		panel.add(userLabel);
+		panel.add(userField);
+		panel.add(pwdLabel);
+		panel.add(pwdField);
+		panel.add(new JLabel(""));
+		panel.add(loginbtn);
+		mainFrame.add(panel);
+		mainFrame.setLocationRelativeTo(null);
+	
+		
+		
+		
+		
+		mainFrame.setVisible(true);
+		
+
+	}
+
+}
